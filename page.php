978d1fff3500eff7d006c7def0c8d4587e8a561a
narimaneaibeche <aibechenarimane@gmail.com>
2023-06-14 23:29:28 +0200

    Integration de la police

--- page.php
+++ page.php
@@ -1,9 +1,11 @@
 <?php get_header(); ?>
+<div id="primary" class="content-area">
+<main id="main" class="site-main" role="main">
 <?php
 /* Start the Loop */
 while ( have_posts() ) :
 	the_post();
-	get_template_part( 'template-parts/content/content-page' );
+	get_template_part( 'template-parts/content','page' );
 
 	// If comments are open or there is at least one comment, load up the comment template.
 	if ( comments_open() || get_comments_number() ) {
@@ -15,3 +17,5 @@ endwhile; // End of the loop.
 
     
 <?php get_footer(); ?>
+
+
