d2adc2151c647a612bb3906112bac912dd07e5ed
narimaneaibeche <aibechenarimane@gmail.com>
2023-06-18 20:22:43 +0200

    L'affichage de la liste des photos

--- index.php
+++ index.php
@@ -9,21 +9,40 @@
         <div class="banner">
            <img src="<?php echo get_template_directory_uri() . '/assets/nathalie-1.jpeg'; ?> " class="hero-event"  alt="image photograpie" > 
        </div>
+       <div class="input">
+       </div>
+
     </div>
-    
+ <!-- affichage des photos -->
+  <div class="liste">
+        <div class="liste-photos">
+            <div id="card2-img-plus" >
+                <div class="card2-img2" >
+                  <?php
+                  $query = new WP_Query([
+                  'post_type' => 'photo',
+                  'posts_per_page' => 8,
+                   ]);?>
+                  <?php
+                   while ($query->have_posts()) : $query->the_post();
+                        ?>
+                       <div class="card-link">
+                            <?php the_post_thumbnail('post-thumbnail');?> 
+                           <?php the_content()?>
+                      </div>
+                   <?php endwhile; ?>
+             </div>
+           </div>  
+      </div>
+      <div class="suite-liste">
+            <button id="btn4"><a>Toutes les photos </a>  </button>
+      </div>
+    </div>
+
+
+
 
 </div>
-<?php
-if (have_posts()): ?>
-<ul>
-    <?php while (have_posts()): the_post();?>
-    <li><?php the_title()?> - <?php the_author() ?> </li>
-    <?php endwhile ?>
-</ul>
-<?php else: ?>
-
-    <h1>pas d'arcticle </h1>
-    <?php endif; ?>
 
 
 
